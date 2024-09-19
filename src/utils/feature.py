349dfa4034484842461f156241ecac745e48fe87
nlwmode <nlwmode@gmail.com>
2024-09-19 08:23:09 +0000

    add feature dim modification

---
+++ src/utils/feature.py
@@ -0,0 +1,25 @@
+import os.path
+import os, sys
+current_dir = os.path.split(os.path.abspath(__file__))[0]
+proj_dir = current_dir.rsplit('/', 2)[0]
+sys.path.append(proj_dir)
+
+import torch
+from torch_geometric.data import Data
+
+def padding_feature_to(graph:Data, feature_size:int):
+    """_summary_
+
+    Args:
+        graph (Data): _description_
+        feature_size (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    pad_size = feature_size - graph.x.shape[1]
+    assert pad_size >= 0, "original feature size is too large"
+    graph.x = graph.x.to(torch.float)
+    additional_features = torch.randn(len(graph.x), pad_size)
+    graph.x = torch.cat((graph.x, additional_features), dim=1)
+    return graph
